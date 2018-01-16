51534e055606d8ce8e33060f7aff30b93ba1c1f2
ali aman <aliaman76@hotmail.com>
2018-01-16 17:01:27 -0500

    Initial commit

---
+++ Dashboard.API/Models/User.cs
@@ -0,0 +1,16 @@
+namespace Dashboard.API.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string FirstName {get;set;}
+        public string LastName {get;set;}
+        public string Address {get;set;}
+        public string City {get; set;}
+        public string Province {get;set;}
+        public string PostalCode {get;set;}
+        public string Email {get;set;}
+        public string Username {get; set;}
+
+    }
+}
